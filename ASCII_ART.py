7462e148b7a62608419b4591879f8d0901818a93
vydrzal.patrik <vydrzal.patrik@spszl.cz>
2025-02-19 09:55:11 +0100

    I am done, I am happy with how this looks, and I need to move on other projects

--- ASCII_ART.py
+++ ASCII_ART.py
@@ -14,19 +14,26 @@ progress_bar = ttk.Progressbar(root, length=250, maximum=250, variable=progress)
 image_dropped = False
 # ____ON_DROP____
 
+def convert():
+    global file_name, file_path, max_width
+    max_width = max_ascii_width.get() or 1000
+    max_width = int(max_width)
+    working_with_picture(str(file_path), file_name)
+
 # ____SETTINGS____
 settings_frame = tk.Frame(root, bg="lightgray")
 convert_button = tk.Button(settings_frame, text="Convert",
-                               command=lambda: working_with_picture(str(file_path), file_name))
+                               command=convert)
 settings_label = tk.Label(settings_frame, text="settings", font=("Arial", 12), bg="lightgray", wraplength=250)
+max_ascii_width = tk.StringVar(value="1000")   # max 1000 char on a line if using win11, 1024 if using win10 notepad
+max_width_label = tk.Label(root, text="Max width:")
+max_width = 0
 # ____SETTINGS____
-
 
 file_name = ""
 ascii_characters_by_surface_10 = " .:-=+*#%@"
 ascii_characters_by_surface_65 = '`^"' + r",:;Il!i~+_-?][}{1)(|\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 ascii_characters_by_surface = ascii_characters_by_surface_10
-
 
 def color_inverter():  # if true, white viewer background is expected(e.g. windows notepad)
     global ascii_characters_by_surface, inverted_colors
@@ -37,7 +44,6 @@ def color_inverter():  # if true, white viewer background is expected(e.g. windo
         ascii_characters_by_surface += char
 
 color_inverter()  # run it once, so the default viewer background is white
-
 
 def pixel_to_ascii(pixel, extension):
     if extension == ".png":
@@ -88,9 +94,8 @@ def pixel_to_ascii(pixel, extension):
         print("I don't support this extension, sry")
         time.sleep(1)
 
-
-def working_with_picture(pic, file_name):
-    global image_dropped
+def working_with_picture(pic, pic_name):
+    global image_dropped, max_width
     extension = os.path.splitext(pic)[-1].lower()
     if extension not in (".png", ".jpeg", ".jpg"):
         print("Bad file extension, need .jpg or .png")
@@ -101,9 +106,9 @@ def working_with_picture(pic, file_name):
 
     # ____RESIZING_IMAGE_TO_BE_VIEWABLE____
     (width, height) = image.size
-    if width > 1024:  # notepad max characters on a line
+    if width > max_width:  # max 1000 char on a line if using win11, 1024 if using win10
         aspect_ratio = width / height
-        width = 1024
+        width = max_width
         height = int(width/aspect_ratio)
     new_height = int(height*0.3676470588235294)  # cos character has greater height than width
     image = image.resize((width, new_height))
@@ -116,8 +121,8 @@ def working_with_picture(pic, file_name):
         progress.set(another_step)
         root.update_idletasks()  # Update the GUI
         line = ""
-        print(another_step)
-        time.sleep(0.001)
+        #print(another_step)
+        #time.sleep(0.001)
         for x in range(width):
             px = image.getpixel((x, y))
             line += pixel_to_ascii(px, extension)
@@ -125,17 +130,15 @@ def working_with_picture(pic, file_name):
         ascii_art.append(line)
     progress.set(0)
     root.update_idletasks()  # Update the GUI after the conversion is done
-    saving_ascii_art(ascii_art, file_name)  # Call the saving_ascii_art function to save the ASCII art
+    saving_ascii_art(ascii_art, pic_name)  # Call the saving_ascii_art function to save the ASCII art
     toggle_settings(False)
     image_dropped = False
 
-
-def saving_ascii_art(ascii_art, file_name):
-    with open(f"{file_name}_ascii_image.txt", "w") as f:
+def saving_ascii_art(ascii_art, pic_name):
+    with open(f"{pic_name}_ascii_image.txt", "w") as f:
         for line in ascii_art:
             f.write(line)
             f.write("\n")
-
 
 def on_drop(event):
     global file_name, image_dropped, file_path, settings_label
@@ -144,7 +147,7 @@ def on_drop(event):
     file_name = os.path.basename(file_path).split(".")[0]  # get only the name of the file and not the extension
     shortened_file_name = ""
     for char in file_name:
-        if len(shortened_file_name) <= 40:
+        if len(shortened_file_name) <= 15:
             shortened_file_name += char
     settings_label.config(text=shortened_file_name)
 
@@ -154,9 +157,8 @@ def on_drop(event):
 
     root.update_idletasks()
 
-
 def toggle_settings(settings_visible = True):
-    global settings_label, inverted_color_checkbox
+    global settings_label, inverted_color_checkbox, width_entry
 
     if not settings_visible:
         # Hide the settings
@@ -164,6 +166,8 @@ def toggle_settings(settings_visible = True):
         convert_button.pack_forget()
         settings_label.pack_forget()
         inverted_color_checkbox.pack_forget()
+        max_width_label.pack_forget()
+        width_entry.pack_forget()
         root.geometry("250x150")  # Adjust the base window size
     else:
         # Show the settings
@@ -171,16 +175,16 @@ def toggle_settings(settings_visible = True):
         settings_frame.pack(fill="both", expand=True)# fill="both", expand=True
         settings_label.pack(pady=10)
         inverted_color_checkbox.pack()
-        root.geometry("250x300")  # Expand the window size to fit settings
+        max_width_label.pack()
+        width_entry.pack()
+        root.geometry("250x304")  # Expand the window size to fit settings
     root.update_idletasks()  # Update the GUI after the conversion is done
 
 
     #settings_visible = not settings_visible
 
-
-
 def main():
-    global inverted_color_checkbox
+    global inverted_color_checkbox, width_entry
 
     root.title("Ascii_art")
     root.geometry("250x150")
@@ -206,7 +210,9 @@ def main():
     root.drop_target_register(DND_FILES)
     root.dnd_bind('<<Drop>>', on_drop)
 
-    inverted_color_checkbox = tk.Checkbutton(settings_frame, text='Inverted colors', command=color_inverter)
+    inverted_color_checkbox = tk.Checkbutton(settings_frame, text='Inverted colors', command=color_inverter,
+                                             background="lightgray")
+    width_entry = tk.Entry(root, textvariable=max_ascii_width)
     root.mainloop()
 
 
